9bdbbe7c319756849f7a2f70d5001bde729d4cbe
Andrea Odetti <mariofutire@gmail.com>
2014-07-02 21:30:28 +0100

    Introduce more structure and functions.
    Andrea

--- pongo/main.lua
+++ pongo/main.lua
@@ -1,12 +1,9 @@
 -- this is the top left
-local paddle_h = 100
-
-local paddle_1 = {}
-local paddle_2 = {}
+local player_1 = {}
+local player_2 = {}
 
 -- this is the centre
 local ball = {}
-ball.r = 15
 
 -- radians
 local random_angle = 0.1
@@ -14,9 +11,6 @@ local random_angle = 0.1
 -- autoplay
 local auto_play = false
 local help_play = false
-
-local points_1 = 0
-local points_2 = 0
 
 
 function bounce(angle, wall, random_angle)
@@ -34,26 +28,26 @@ end
 
 -- the collision is only wrt the main side of the paddle
 -- so we are checking intersection between a segment of a line and a circle
-function collision(paddle)
-   local m = math.tan(paddle.angle)
+function collision(player)
+   local m = math.tan(player.angle)
    local a = -m
    local b = 1
-   local c = m * paddle.x - paddle.y
+   local c = m * player.x - player.y
 
    local collision = false
 
    local dist_fromline = math.abs(a * ball.x + b * ball.y + c) / math.sqrt(a * a + b * b)
    if dist_fromline < ball.r then
-      local dist_fromcentre = math.sqrt((paddle.x - ball.x) ^ 2 + (paddle.y - ball.y) ^ 2)
-      if dist_fromcentre < paddle_h / 2 then
+      local dist_fromcentre = math.sqrt((player.x - ball.x) ^ 2 + (player.y - ball.y) ^ 2)
+      if dist_fromcentre < player.height / 2 then
 	 collision = true
       end
    end
 
    -- after a collision we do not return an other collision
    -- until after a non collision has happened
-   local old_collision = paddle.collision
-   paddle.collision = collision
+   local old_collision = player.collision
+   player.collision = collision
 
    return (not old_collision) and collision
 end
@@ -63,26 +57,55 @@ function setup()
    -- 100 pixels are the "goal" zone on each side
    min_of_game = 100
    max_of_game = width - min_of_game
-
-   paddle_1.x = min_of_game
-   paddle_1.y = height / 2
-   paddle_1.speed_x = 200
-   paddle_1.speed_y = 500
-
-   paddle_1.coeff_x = 0
-   paddle_1.coeff_y = 0
-   paddle_1.angle = math.pi / 2
-
-   paddle_2.x = max_of_game
-   paddle_2.y = height / 2
-   paddle_2.speed_x = 200
-   paddle_2.speed_y = 500
-
-   paddle_2.coeff_x = 0
-   paddle_2.coeff_y = 0
-   paddle_2.angle = math.pi / 2
-
    scene_angle = 0
+
+   local paddle_h = 100
+
+   ball.r = 15
+   ball.color = {204, 204, 0}
+   ball.min_x = min_of_game + ball.r
+   ball.max_x = max_of_game - ball.r
+   ball.min_y = ball.r
+   ball.max_y = height - ball.r
+
+   -- player 1
+   player_1.points = 0
+   player_1.color = {255, 0, 0}
+
+   player_1.x = min_of_game
+   player_1.y = height / 2
+   player_1.speed_x = 200
+   player_1.speed_y = 500
+
+   player_1.min_y = 0
+   player_1.max_y = height
+   player_1.min_x = min_of_game
+   player_1.max_x = width / 2
+   player_1.height = paddle_h
+   player_1.width = 10
+
+   player_1.coeff_x = 0
+   player_1.coeff_y = 0
+   player_1.angle = math.pi / 2
+
+   -- player 2
+   player_2.points = 0
+   player_2.color = {255, 255, 0}
+
+   player_2.x = max_of_game
+   player_2.y = height / 2
+   player_2.speed_x = 200
+   player_2.speed_y = 500
+   player_2.min_y = 0
+   player_2.max_y = height
+   player_2.min_x = width / 2
+   player_2.max_x = max_of_game
+   player_2.height = paddle_h
+   player_2.width = -10
+
+   player_2.coeff_x = 0
+   player_2.coeff_y = 0
+   player_2.angle = math.pi / 2
 
    restart()
 end
@@ -98,28 +121,56 @@ function restart()
 
    ball.angle = (math.random() - 0.5) * math.pi / 2
 
-   paddle_1.collision = false
-   paddle_2.collision = false
+   -- reset collision
+   player_1.collision = false
+   player_2.collision = false
+   -- leave players where they are
+end
+
+
+function ball_draw(ball)
+   love.graphics.setColor(ball.color)
+   love.graphics.circle('fill', ball.x, ball.y, ball.r, 10)
+end
+
+
+function player_draw(player)
+   love.graphics.setColor(player.color)
+   love.graphics.push()
+   love.graphics.translate(player.x, player.y)
+   love.graphics.rotate(player.angle)
+   love.graphics.rectangle('fill', -player.height / 2, 0, player.height, player.width)
+   love.graphics.pop()
+end
+
+
+function player_update(player, dt)
+   player.x = player.x + player.coeff_x * player.speed_x * dt
+   player.y = player.y + player.coeff_y * player.speed_y * dt
+   player.x = bound(player.x, player.min_x, player.max_x)
+   player.y = bound(player.y, player.min_y, player.max_y)
+end
+
+
+function court_draw()
+   love.graphics.setColor(255, 255, 0)
+   love.graphics.line(min_of_game, 0, min_of_game, height)
+   love.graphics.line(max_of_game, 0, max_of_game, height)
+   love.graphics.line(width / 2, height * 2 / 5 , width / 2, height * 3 / 5)
+   love.graphics.rectangle('line', 0, 0, width, height)
+   love.graphics.circle('line', width / 2, height / 2, 10, 10)
 end
 
 
 function love.load()
    math.randomseed(os.time())
 
+   love.keyboard.setKeyRepeat(true)
+
    width = love.graphics.getWidth()
    height = love.graphics.getHeight()
 
    setup()
-
-   love.keyboard.setKeyRepeat(true)
-
-   ball.min_x = min_of_game + ball.r
-   ball.max_x = max_of_game - ball.r
-   ball.min_y = ball.r
-   ball.max_y = height - ball.r
-
-   paddle_min_y = 0
-   paddle_max_y = height
 
    font = love.graphics.newFont(40)
    love.graphics.setFont(font)
@@ -138,36 +189,33 @@ function love.update(dt)
 
    if auto_play then
       -- this line makes it plays automatically
-      -- paddle_2.y = ball.y
-      paddle_1.y = ball.y
+      -- player_2.y = ball.y
+      player_1.y = ball.y
    else
-      paddle_1.x = paddle_1.x + paddle_1.coeff_x * paddle_1.speed_x * dt
-      paddle_1.y = paddle_1.y + paddle_1.coeff_y * paddle_1.speed_y * dt
-      paddle_1.x = bound(paddle_1.x, min_of_game, width / 2)
-      paddle_1.y = bound(paddle_1.y, paddle_min_y, paddle_max_y)
+      player_update(player_1, dt)
    end
 
-   paddle_2.coeff_x = joystick:getAxis(1)
-   paddle_2.coeff_y = joystick:getAxis(2)
-   paddle_2.angle = (joystick:getAxis(3) * 0.9 + 1) * math.pi / 2
+   if joystick then
+      player_2.coeff_x = joystick:getAxis(1)
+      player_2.coeff_y = joystick:getAxis(2)
+      player_2.angle = (joystick:getAxis(3) * 0.9 + 1) * math.pi / 2
+   end
 
-   paddle_2.x = paddle_2.x + paddle_2.coeff_x * paddle_2.speed_x * dt
-   paddle_2.y = paddle_2.y + paddle_2.coeff_y * paddle_2.speed_y * dt
-   paddle_2.x = bound(paddle_2.x, width / 2, max_of_game)
-   paddle_2.y = bound(paddle_2.y, paddle_min_y, paddle_max_y)
+   player_update(player_2, dt)
 
-   if collision(paddle_2) then
-      ball.angle = bounce(ball.angle, paddle_2.angle, random_angle)
-   elseif collision(paddle_1) then
-      ball.angle = bounce(ball.angle, paddle_1.angle, random_angle)
+   if collision(player_2) then
+      ball.angle = bounce(ball.angle, player_2.angle, random_angle)
+   elseif collision(player_1) then
+      ball.angle = bounce(ball.angle, player_1.angle, random_angle)
    elseif ball.x > ball.max_x then
-      points_1 = points_1 + 1
+      player_1.points = player_1.points + 1
       restart()
    elseif ball.x < ball.min_x then
-      points_2 = points_2 + 1
+      player_2.points = player_2.points + 1
       restart()
    end
 
+   -- bounce up and down
    if ball.y < ball.min_y or ball.y > ball.max_y then
       -- no random angle added here
       ball.angle = bounce(ball.angle, 0, 0)
@@ -179,7 +227,6 @@ end
 
 function love.draw()
    -- rotate around the center of the screen by angle radians
-
    love.graphics.translate(width / 2, height / 2)
    love.graphics.rotate(scene_angle)
    love.graphics.translate(-width / 2, -height / 2)
@@ -191,50 +238,31 @@ function love.draw()
       love.graphics.line(ball.x, ball.y, ball.x + dx * 1, ball.y + dy * 1)
    end
 
-   -- court lines
+   -- court
+   court_draw()
 
-   love.graphics.setColor(255, 255, 0)
-   love.graphics.line(min_of_game, 0, min_of_game, height)
-   love.graphics.line(max_of_game, 0, max_of_game, height)
-   love.graphics.line(width / 2, height * 2 / 5 , width / 2, height * 3 / 5)
-   love.graphics.rectangle('line', 0, 0, width, height)
-   love.graphics.circle('line', width / 2, height / 2, 10, 10)
-
-   -- paddles
-
-   love.graphics.setColor(255, 0, 0)
-   love.graphics.push()
-   love.graphics.translate(paddle_2.x, paddle_2.y)
-   love.graphics.rotate(paddle_2.angle)
-   love.graphics.rectangle('fill', -paddle_h / 2, 0, paddle_h, -10)
-   love.graphics.pop()
-
-   love.graphics.setColor(255, 255, 0)
-   love.graphics.push()
-   love.graphics.translate(paddle_1.x, paddle_1.y)
-   love.graphics.rotate(paddle_1.angle)
-   love.graphics.rectangle('fill', -paddle_h / 2, 0, paddle_h, 10)
-   love.graphics.pop()
+   -- players
+   player_draw(player_1)
+   player_draw(player_2)
 
    -- ball
+   ball_draw(ball)
 
-   love.graphics.setColor(204, 204, 0)
-   love.graphics.circle('fill', ball.x, ball.y, ball.r, 10)
-
+   -- points
    love.graphics.setColor(123, 204, 40)
-   love.graphics.print(points_1 .. " : " .. points_2, 300, 300)
+   love.graphics.print(player_1.points .. " : " .. player_2.points, 300, 300)
 end
 
 
 function love.keypressed(key)
    if key == "e" then
-      paddle_1.coeff_y = -1
+      player_1.coeff_y = -1
    elseif key == "d" then
-      paddle_1.coeff_y = 1
+      player_1.coeff_y = 1
    elseif key =="k" then
-      paddle_1.angle = paddle_1.angle + 0.1
+      player_1.angle = player_1.angle + 0.1
    elseif key =="l" then
-      paddle_1.angle = paddle_1.angle - 0.1
+      player_1.angle = player_1.angle - 0.1
 
    elseif key == "up" then
       ball.speed = ball.speed + 10
@@ -254,9 +282,9 @@ end
 
 function love.keyreleased(key)
    if key == "e" then
-      paddle_1.coeff_y = 0
+      player_1.coeff_y = 0
    elseif key == "d" then
-      paddle_1.coeff_y = 0
+      player_1.coeff_y = 0
    end
 end
 
