810e0f9e2f2ff2e0fbacc74ef3fa3d3e68e7f03f
Andrea Odetti <mariofutire@gmail.com>
2017-04-17 19:09:11 +0100

    More correct implementation of the centroid.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- car/geometry.lua
+++ car/geometry.lua
@@ -2,6 +2,33 @@ local vector = require("vector")
 
 local M = {}
 
+local function centroid2d(vertices, x, y)
+   local s_x = 0
+   local s_y = 0
+   local s_a = 0
+
+   local n = #vertices
+
+   for i = 1, n do
+      local x_i = vertices[i][x]
+      local y_i = vertices[i][y]
+
+      local x_i1 = ((i < n) and vertices[i + 1][x]) or vertices[1][x]
+      local y_i1 = ((i < n) and vertices[i + 1][y]) or vertices[1][y]
+
+      local common = x_i * y_i1 - x_i1 * y_i
+      s_x = s_x + (x_i + x_i1) * common
+      s_y = s_y + (y_i + y_i1) * common
+      s_a = s_a + common
+   end
+
+   if s_a == 0 then
+      return nil, nil
+   else
+      return s_x / (3 * s_a), s_y / (3 * s_a)
+   end
+end
+
 local function normal(vertices)
    local a = vector.add(vertices[3], -1, vertices[2])
    local b = vector.add(vertices[1], -1, vertices[2])
@@ -10,11 +37,9 @@ local function normal(vertices)
    return res
 end
 
-local function centroid(vertices, k, h)
+local function average(vertices)
    local res = vector.empty()
 
-   -- this is correct only for RECTANGLES
-   -- needs to be fixed
    for _, vertex in ipairs(vertices) do
       res = vector.add(res, 1, vertex)
    end
@@ -24,6 +49,25 @@ local function centroid(vertices, k, h)
    res[1] = res[1] / n
    res[2] = res[2] / n
    res[3] = res[3] / n
+
+   return res
+end
+
+local function centroid(vertices)
+   local res = average(vertices)
+
+   -- for triangles the centroid is the average
+   -- which we use as a backup in case the following method does not work
+
+   if #vertices > 3 then
+      local c_x1, c_y1 = centroid2d(vertices, 1, 2)
+      local c_y2, c_z1 = centroid2d(vertices, 2, 3)
+      local c_z2, c_x2 = centroid2d(vertices, 3, 1)
+
+      res[1] = (c_x1 or c_x2) or res[1]
+      res[2] = (c_y1 or c_y2) or res[2]
+      res[3] = (c_z1 or c_z2) or res[3]
+   end
 
    return res
 end
