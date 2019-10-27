3bc8814f3879ca2ee549eadcb1dfb27d4d568875
Andrea Odetti <mariofutire@gmail.com>
2019-10-27 08:36:19 +0000

    Update colorspace to love 0.11
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- car/colors.lua
+++ car/colors.lua
@@ -1,20 +1,20 @@
 local M = {}
 
-M.red       = {255,   0,   0}
-M.lime      = {  0, 255,   0}
-M.blue      = {  0,   0, 255}
-M.black     = {  0,   0,   0}
-M.white     = {255, 255, 255}
-M.yellow    = {255, 255,   0}
-M.cyan      = {  0, 255, 255}
-M.magenta   = {255,   0, 255}
-M.silver    = {192, 192, 192}
-M.gray      = {128, 128, 128}
-M.maroon    = {128,   0,   0}
-M.olive     = {128, 128,   0}
-M.green     = {  0, 128,   0}
-M.purple    = {128,   0, 128}
-M.teal      = {  0, 128, 128}
-M.navy      = {  0,   0, 128}
+M.red       = {1, 0, 0}
+M.lime      = {0, 1, 0}
+M.blue      = {0, 0, 1}
+M.black     = {0, 0, 0}
+M.white     = {1, 1, 1}
+M.yellow    = {1, 1, 0}
+M.cyan      = {0, 1, 1}
+M.magenta   = {1, 0, 1}
+M.silver    = {0.75, 0.75, 0.75}
+M.gray      = {0.5, 0.5, 0.5}
+M.maroon    = {0.5, 0, 0}
+M.olive     = {0.5, 0.5, 0}
+M.green     = {0, 0.5, 0}
+M.purple    = {0.5, 0, 0.5}
+M.teal      = {0, 0.5, 0.5}
+M.navy      = {0, 0, 0.5}
 
 return M
